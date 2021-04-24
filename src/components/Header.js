8db9a1e8807e2b5edb1748989f94ac523261f02d
Saravana Raja <saravanaraja25@gmail.com>
2021-04-24 11:39:22 +0530

    menu toggle fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react';
+import React,{useEffect,useState} from 'react';
 import {Nav,Navbar,NavDropdown} from 'react-bootstrap';
 import {NavLink,Link,useLocation } from 'react-router-dom';
 import logo1 from '../assets/img/logo1.jpg'
@@ -6,6 +6,7 @@ import logo1 from '../assets/img/logo1.jpg'
 const Header = () => {
     const location = useLocation();
     const [scrolled,setScrolled]=React.useState(false);
+    const [expanded,setExpanded]=useState(false)
     const handleScroll=() => {
         const offset=window.scrollY;
         if(offset > 100 ){
@@ -31,27 +32,27 @@ const Header = () => {
                     </div>
                 </div>
             </div>
-            <Navbar className={"main-navbar " +(scrolled ? 'fixed-top' : '')} bg="white" expand="lg">
+            <Navbar collapseOnSelect expanded={expanded} className={"main-navbar " +(scrolled ? 'fixed-top' : '')} bg="white" expand="lg">
                 <div className="container">
-                <NavLink to="/" exact className="navbar-brand d-flex">
+                <NavLink to="/" onClick={()=>setExpanded(false)} exact className="navbar-brand d-flex">
                     <img src={logo1} alt="" />
                     <div className="d-block p-3">
                         <h5 className="text-dark">Kongu Engineering College</h5>
                         <h6 className="text-dark">M.Sc Software Systems (5 Years)</h6>
                     </div>
                 </NavLink>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse className="pb-2" id="basic-navbar-nav">
+                    <Navbar.Toggle onClick={()=>setExpanded(!expanded)} aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse className="pb-2" id="responsive-navbar-nav">
                         <Nav className="ml-auto">
-                            <NavLink className="nav-link" to="/" exact activeClassName="active"><span>Home</span></NavLink>
-                            <NavLink className="nav-link" to="/about" exact activeClassName="active"><span>About</span></NavLink>
-                            <NavDropdown title="Academics" className={"" +(((location.pathname==='/placement')||(location.pathname==='/whymsc')||(location.pathname==='/curriculum')) ? 'activedrop' : '')} id="basic-nav-dropdown">
-                                <Link className="dropdown-item" to="/whymsc" exact><span>Why MSc @ KEC</span></Link>
-                                <Link className="dropdown-item" to="/curriculum" exact><span>Curriculum</span></Link>
-                                <Link className="dropdown-item" to="/placement" exact><span>Placements</span></Link>
+                            <NavLink className="nav-link" onClick={()=>setExpanded(false)} to="/" exact activeClassName="active"><span>Home</span></NavLink>
+                            <NavLink className="nav-link" onClick={()=>setExpanded(false)} to="/about" exact activeClassName="active"><span>About</span></NavLink>                            
+                            <NavDropdown title="Academics"  className={"" +(((location.pathname==='/placement')||(location.pathname==='/whymsc')||(location.pathname==='/curriculum')) ? 'activedrop' : '')} id="basic-nav-dropdown">
+                                <NavDropdown.Item className="p-0"><Link onClick={()=>setExpanded(false)} className="dropdown-item" to="/whymsc" exact><span>Why MSc @ KEC</span></Link></NavDropdown.Item>
+                                <NavDropdown.Item className="p-0"><Link onClick={()=>setExpanded(false)} className="dropdown-item" to="/curriculum" exact><span>Curriculum</span></Link></NavDropdown.Item>
+                                <NavDropdown.Item className="p-0"><Link onClick={()=>setExpanded(false)} className="dropdown-item" to="/placement" exact><span>Placements</span></Link></NavDropdown.Item>
                             </NavDropdown>
-                            <NavLink className="nav-link" to="/contact" exact activeClassName="active"><span>Contact</span></NavLink>
-                            <NavLink className="nav-link" to="/admission" exact activeClassName="active"><span>Admission</span></NavLink>                            
+                            <NavLink className="nav-link" onClick={()=>setExpanded(false)} to="/contact" exact activeClassName="active"><span>Contact</span></NavLink>
+                            <NavLink className="nav-link" onClick={()=>setExpanded(false)} to="/admission" exact activeClassName="active"><span>Admission</span></NavLink>                            
                         </Nav>
                     </Navbar.Collapse>
                 </div>
